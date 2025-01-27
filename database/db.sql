e04467d00bae63bb7132eae64278e7e2f62de93a
noumanc <nouman.c@crewlogix.com>
2025-01-27 16:32:56 +0500

    first commit

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+-- creating the database
+CREATE DATABASE crudnodejsmysql;
+
+use crudnodejsmysql;
+
+-- creating a table
+CREATE TABLE customer (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15),
+    age VARCHAR(18)
+);
+
+-- Show all tables
+SHOW TABLES;
+
+-- To describe the table
+describe customer;
